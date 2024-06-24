9446ead9f13ab2404ecd562d18b615199cec1492
Теньгу <163237085+ManagoL@users.noreply.github.com>
2024-06-24 17:55:07 +0300

    Add files via upload

---
+++ Личный кабинет/CabinetScript.js
@@ -0,0 +1,5 @@
+document.querySelector('input[type="file"]').onchange = event => {
+    let reader = new FileReader();
+    reader.onload = e => document.querySelector('img').src = e.target.result;
+    reader.readAsDataURL(event.target.files[0]);
+};
