046d4d9bc8d0810590b14966dd9c3fa87b01ce33
Теньгу <163237085+ManagoL@users.noreply.github.com>
2024-06-26 17:15:56 +0300

    Delete Личный кабинет directory

--- Личный кабинет/CabinetScript.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('input[type="file"]').onchange = event => {
-    let reader = new FileReader();
-    reader.onload = e => document.querySelector('img').src = e.target.result;
-    reader.readAsDataURL(event.target.files[0]);
-};
